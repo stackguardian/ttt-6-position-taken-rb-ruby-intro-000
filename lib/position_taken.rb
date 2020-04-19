dc04ed4525f17dd760951a29f3e1641b2308d218
Muhtasim Ferdous <mferdous98@aol.com>
2020-04-19 22:39:38 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,5 +2,9 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 def position_taken?(board, index)
-board[index] != " "  &&  board[index] != "" && board[index] != nil
+if board[index] == "" || board[index] == " " || board[index] == nil
+  false
+else
+  true
+end
 end
